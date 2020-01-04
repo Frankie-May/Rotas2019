adbd76159a70839b69f489a686832237fbc3f06d
Frankie-May <Francisco.May@tecnico.ulisboa.pt>
2020-01-04 00:10:09 +0000

    Ficheiros que fizemos até agora!

--- rotas2019.c
+++ rotas2019.c
@@ -1,9 +1,21 @@
 #include "airport.h"
 
 int main(int argc, char * argv[]){
-	printf("%s %s \n", argv[1], argv[2]);
+	/*printf("%s %s \n", argv[1], argv[2]);*/
 
+	char *test;
+	FILE* fp;
 
+	strcpy(test, "Ficheiro_dos_aeroportos.txt");
+	fp = fopen(test , "r");
+	if (fp == NULL)
+	{
+		printf("ERROR: Error opening file: %s! :-(\n", test);
+		return -1;
+	}
+
+	fclose(fp);
+	fprintf(stdout, "%s\n", "End of File! :-)");
 
 	return 0;
 }
